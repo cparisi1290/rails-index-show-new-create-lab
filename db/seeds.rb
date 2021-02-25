942059fcecb6c486dfff75de157707dde6c5e294
Cassandra <cparisi1290@gmail.com>
2021-02-25 16:23:58 -0500

    commit tree e2aa1a7ba819839546c6db44888b65b42cb8bd02

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+chip = Coupon.create(coupon_code: "ASD123", store: "Chipotle")
+jam = Coupon.create(coupon_code: "XYZ098", store: "Jamba")
